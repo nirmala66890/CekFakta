7184dc2eaf5952e40db4ee5375b3a3bfe61d5b9b
Ziko Zeriko <131319852+nirmala66890@users.noreply.github.com>
2024-12-11 16:05:14 +0700

    Delete Mobile_Development directory

--- Mobile_Development/app/src/main/java/com/dicoding/capstone_newschecker/data/NewsData.kt
+++
@@ -1,6 +0,0 @@
-package com.dicoding.capstone_newschecker.data
-
-data class NewsData(
-    val isReal: Boolean,
-    val message: String
-)
--- Mobile_Development/app/src/main/java/com/dicoding/capstone_newschecker/data/NewsPrediction.kt
+++
@@ -1,5 +0,0 @@
-package com.dicoding.capstone_newschecker.data
-
-data class NewsPrediction(
-    val prediction: Int // 1 untuk benar, 0 untuk hoax
-)
